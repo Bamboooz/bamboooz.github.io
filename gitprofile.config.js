9aca66184425651f022bd69c0c4eae5c86f7a4b2
Bamboooz <bambusixmc@gmail.com>
2023-03-31 17:54:00 +0200

    Initial commit

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,12 +2,12 @@
 
 const config = {
   github: {
-    username: 'Bamboooz', // Your GitHub org/user name. (Required)
-    sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    username: 'Bamboooz',
+    sortBy: 'stars',
+    limit: 10,
     exclude: {
-      forks: true, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      forks: true,
+      projects: ['bamboooz.github.io'],
     },
   },
   social: {
@@ -20,7 +20,7 @@ const config = {
     behance: '',
     medium: '',
     dev: '',
-    stackoverflow: '', // format: userid/username
+    stackoverflow: '',
     skype: '',
     telegram: '',
     website: 'https://bamboooz.github.io',
@@ -29,7 +29,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
   },
   skills: [
     'Python',
@@ -39,48 +39,12 @@ const config = {
     'Assembly',
     '.Net'
   ],
-  experiences: [],
-  /* certifications: [
-    {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com'
-    },
-  ], */
-  education: [],
-
-  // To hide the `My Projects` section, keep it empty.
-  externalProjects: [],
-  // Display blog posts from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: '', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
-  },
-  googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-  },
-  // Track visitor interaction and behavior. https://www.hotjar.com
-  hotjar: {
-    id: '',
-    snippetVersion: 6,
-  },
   themeConfig: {
     defaultTheme: 'dark',
-
-    // Hides the switch in the navbar
-    // Useful if you want to support a single color mode
     disableSwitch: false,
-
-    // Should use the prefers-color-scheme media-query,
-    // using user system preferences, instead of the hardcoded defaultTheme
     respectPrefersColorScheme: false,
-
-    // Hide the ring in Profile picture
     hideAvatarRing: false,
 
-    // Available themes. To remove any theme, exclude from here.
     themes: [
       'dark',
       'light',
@@ -114,7 +78,6 @@ const config = {
       'procyon',
     ],
 
-    // Custom theme
     customTheme: {
       primary: '#fc055b',
       secondary: '#219aaf',
@@ -126,7 +89,6 @@ const config = {
     },
   },
 
-  // Optional Footer. Supports plain text or HTML.
   footer: `Buy me a coffe <a 
       class="text-primary" href="https://github.com/Bamboooz"
       target="_blank"
