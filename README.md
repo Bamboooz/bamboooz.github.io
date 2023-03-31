878119686adbcc47a1d043c37349f3b21e197572
Bamboooz <bambusixmc@gmail.com>
2023-03-31 17:47:12 +0200

    some tests

---
+++ README.md
@@ -0,0 +1,2 @@
+My portfolio, made using GitProfile, which was made by [arifszn](https://github.com/arifszn)
+
---
+++ gitprofile.config.js
@@ -0,0 +1,138 @@
+// gitprofile.config.js
+
+const config = {
+  github: {
+    username: 'Bamboooz', // Your GitHub org/user name. (Required)
+    sortBy: 'stars', // stars | updated
+    limit: 10, // How many projects to display.
+    exclude: {
+      forks: true, // Forked projects will not be displayed if set to true.
+      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+    },
+  },
+  social: {
+    linkedin: '',
+    twitter: '',
+    mastodon: '',
+    facebook: '',
+    instagram: '',
+    dribbble: '',
+    behance: '',
+    medium: '',
+    dev: '',
+    stackoverflow: '', // format: userid/username
+    skype: '',
+    telegram: '',
+    website: 'https://bamboooz.github.io',
+    phone: '',
+    email: 'bambusixmc@gmail.com',
+  },
+  resume: {
+    fileUrl:
+      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+  },
+  skills: [
+    'Python',
+    'Java',
+    'C#',
+    'C',
+    'Assembly',
+    '.Net'
+  ],
+  experiences: [],
+  /* certifications: [
+    {
+      name: 'Lorem ipsum',
+      body: 'Lorem ipsum dolor sit amet',
+      year: 'March 2022',
+      link: 'https://example.com'
+    },
+  ], */
+  education: [],
+
+  // To hide the `My Projects` section, keep it empty.
+  externalProjects: [],
+  // Display blog posts from your medium or dev account. (Optional)
+  blog: {
+    source: 'dev', // medium | dev
+    username: '', // to hide blog section, keep it empty
+    limit: 2, // How many posts to display. Max is 10.
+  },
+  googleAnalytics: {
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+  },
+  // Track visitor interaction and behavior. https://www.hotjar.com
+  hotjar: {
+    id: '',
+    snippetVersion: 6,
+  },
+  themeConfig: {
+    defaultTheme: 'dark',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: false,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Hide the ring in Profile picture
+    hideAvatarRing: false,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'dark',
+      'light',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'procyon',
+    ],
+
+    // Custom theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+
+  // Optional Footer. Supports plain text or HTML.
+  footer: `Buy me a coffe <a 
+      class="text-primary" href="https://github.com/Bamboooz"
+      target="_blank"
+      rel="noreferrer"
+    >here</a> ☕.`,
+};
+
+export default config;
+
