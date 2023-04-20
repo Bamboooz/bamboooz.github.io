32657b04e92f718cd6774b07bbcc4afb3020b989
Bamboooz <83116089+Bamboooz@users.noreply.github.com>
2023-04-20 11:39:57 +0200

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -92,7 +92,7 @@ const config = {
   },
 
   footer: `Buy me a coffe <a 
-      class="text-primary" href="https://github.com/Bamboooz"
+      class="text-primary" href="https://www.buymeacoffee.com/Bamboooz"
       target="_blank"
       rel="noreferrer"
     >here</a> ☕.`,
